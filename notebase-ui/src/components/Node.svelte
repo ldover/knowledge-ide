6513cbd3944d5eb4adbd5eaaf08949ee0baa1a49
ldover <luka.dover@ultraworking.com>
2022-06-10 11:57:27 +0100

    Fix Svelte rendering bug by adding id to each node

--- notebase-ui/src/components/Node.svelte
+++ notebase-ui/src/components/Node.svelte
@@ -1,8 +1,22 @@
 <script>
   import {Components, injectComponents} from "../util";
   import Node from "./Node.svelte";
+  import {beforeUpdate} from "svelte";
 
   export let node;
+  let prevNode;
+
+  const addId = (node) => node.id = Math.round(Math.random() * 100000000) + '';
+  beforeUpdate(() => {
+    if (prevNode === node) return;
+
+    prevNode = node;
+
+    node.children && node.children.map(addId);
+  })
+
+
+
   let component = Components[node.type];
 
   console.log({node, component})
@@ -10,7 +24,7 @@
 
 <svelte:component this={component} node={node}>
     {#if Array.isArray(node.children)}
-        {#each node.children as childNode}
+        {#each node.children as childNode (childNode.id)}
             <Node node={childNode}/>
         {/each}
     {/if}
