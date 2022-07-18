6ddfe07f0eb504944b1d2902a6b6a1baaecfceee
ldover <luka.dover@ultraworking.com>
2022-07-18 10:43:43 +0100

    Add a statement symbol / link

--- notebase-ui/src/components/Statement.svelte
+++ notebase-ui/src/components/Statement.svelte
@@ -1,5 +1,8 @@
 <script>
+  import {sNavigation} from "../store";
+
   export let node;
+  export let root;
 
   console.log('Statement', {node})
 </script>
@@ -7,10 +10,21 @@
 
 <!-- TODO: consider adding a link to navigate to the statement and examine backlinks -->
 <div class="statement relative">
+    {#if !root}
+        <a class="note-handle cursor-pointer absolute text-orange-500 text-xs font-medium"
+           on:click|stopPropagation={() => sNavigation.set(node.note)}
+        >
+            {node.note.title} →
+        </a>
+    {/if}
     <slot></slot>
 </div>
 
 <style>
+    .note-handle {
+        top: 0;
+        right: 0;
+    }
 
 </style>
 
