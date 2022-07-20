5a7fdaf7815e3c494268d4ab6b2046325c914a81
ldover <luka.dover@ultraworking.com>
2022-07-20 18:47:33 +0100

    Add test for the whole script

--- kdl-parser/test/index.test.js
+++ kdl-parser/test/index.test.js
@@ -199,3 +199,128 @@ describe('Parses nested statement', () => {
     expect(parse(kdl0)).toMatchObject(out);
   });
 })
+
+describe('Parser works for the entire KDL script', () => {
+  it('Parses two defs and and 2 top-level statements', () => {
+    const kdl0 = `def Action as A
+def Quality as Q
+
+1: {A} in the limit converges towards {Q}
+  1.1: Child statement
+  1.2: Child statement 2
+2: {Q} mediates {A}
+  2.1: Child statement
+  2.2: Child statement 2
+    `
+    const out = {
+      type: "root",
+      children: [
+        {
+          type: 'symbol',
+          name: 'Action',
+          abbreviation: 'A'
+        },
+        {
+          type: 'symbol',
+          name: 'Quality',
+          abbreviation: 'Q'
+        },
+        {
+          type: 'statement',
+          name: '1',
+          value: [
+            {
+              type: 'reference',
+              value: 'A'
+            },
+            {
+              type: 'text',
+              value: ' in the limit converges towards '
+            },
+            {
+              type: 'reference',
+              value: 'Q'
+            }
+          ],
+          children: [
+            {
+              type: 'statement',
+              name: '1',
+              nested: true,
+              parentName: '1',
+              value: [
+                {
+                  type: 'text',
+                  value: 'Child statement'
+                }
+              ],
+              children: []
+            },
+            {
+              type: 'statement',
+              name: '2',
+              nested: true,
+              parentName: '1',
+              value: [
+                {
+                  type: 'text',
+                  value: 'Child statement 2'
+                }
+              ],
+              children: []
+            }
+          ]
+        },
+        {
+          type: 'statement',
+          name: '2',
+          value: [
+            {
+              type: 'reference',
+              value: 'Q'
+            },
+            {
+              type: 'text',
+              value: ' mediates '
+            },
+            {
+              type: 'reference',
+              value: 'A'
+            }
+          ],
+          children: [
+            {
+              type: 'statement',
+              name: '1',
+              nested: true,
+              parentName: '2',
+              value: [
+                {
+                  type: 'text',
+                  value: 'Child statement'
+                }
+              ],
+              children: []
+            },
+            {
+              type: 'statement',
+              name: '2',
+              nested: true,
+              parentName: '2',
+              value: [
+                {
+                  type: 'text',
+                  value: 'Child statement 2'
+                }
+              ],
+              children: []
+            }
+          ]
+        }
+      ]
+    }
+
+    let actual = parse(kdl0);
+    expect(actual).toMatchObject(out);
+  });
+})
